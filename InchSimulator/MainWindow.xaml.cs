0c3a83221af94320a3d05039c80e591acf16cb5b
squallcloud <squallcloudzidane@gmail.com>
2020-12-06 20:17:00 +0900

    2種類のターゲットインチの枠線を表示するようにした

--- InchSimulator/MainWindow.xaml.cs
+++ InchSimulator/MainWindow.xaml.cs
@@ -20,6 +20,8 @@ namespace InchSimulator
     /// </summary>
     public partial class MainWindow : Window
     {
+        double m_BaseInch = 0.0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -27,7 +29,48 @@ namespace InchSimulator
 
         private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
         {
+            var textBox = sender as TextBox;
 
+            if (double.TryParse(textBox.Text, out double baseInch)) {
+                m_BaseInch = baseInch;
+            }
+        }
+
+        private void TargetInch1TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            if (double.TryParse(textBox.Text, out double targetInch))
+            {
+                double ratio = targetInch / m_BaseInch;
+
+                double w = SystemParameters.PrimaryScreenWidth;
+                double h = SystemParameters.PrimaryScreenHeight;
+
+                //rectGeom.Rect = new Rect(0, 0, ratio, ratio);
+
+                TargetBorder1.Width = w * ratio;
+                TargetBorder1.Height = h * ratio;
+            }
+        }
+
+        private void TargetInch2TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var textBox = sender as TextBox;
+
+            if (double.TryParse(textBox.Text, out double targetInch))
+            {
+                double ratio = targetInch / m_BaseInch;
+
+                double w = SystemParameters.PrimaryScreenWidth;
+                double h = SystemParameters.PrimaryScreenHeight;
+
+                //rectGeom.Rect = new Rect(0, 0, ratio, ratio);
+
+
+                TargetBorder2.Width = w * ratio;
+                TargetBorder2.Height = h * ratio;
+            }
         }
 
         private void TextBlock_Loaded(object sender, RoutedEventArgs e)
@@ -38,6 +81,30 @@ namespace InchSimulator
             var textBlock = sender as TextBlock;
 
             textBlock.Text = $"{(int)w}x{(int)h}";
+        }
+
+        private void TargetInch1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(int.TryParse(e.Text, out int result) || e.Text == "."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void TargetInch2_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(int.TryParse(e.Text, out int result) || e.Text == "."))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void BaseInch_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            if (!(int.TryParse(e.Text, out int result) || e.Text == "."))
+            {
+                e.Handled = true;
+            }
         }
     }
 }
