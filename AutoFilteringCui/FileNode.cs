56f7c9b35ac190781a51dcb378bbb1ccde34edcd
squallcloud <squallcloudzidane@gmail.com>
2025-04-30 00:02:44 +0900

    Workbranch (#3)
    * [add] CustomBuild 追加
    
    * [update] ファイル、フィルタを管理するためのノードクラス追加
    
    * [update] 各処理を実装
    
    * [update] PackageReference に変更
    
    * [update] .NET8 で作成
    
    * [update] launchSettings.json は除外
    
    * [update] .NET に置き換えたので CustomBuild は削除

---
+++ AutoFilteringCui/FileNode.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Build.Evaluation;
+
+namespace AutoFilterginCui
+{
+    class FileNode
+    {
+        public string Path {
+            get;
+            set;
+        } = "";
+
+        public string Name {
+            get;
+            set;
+        } = "";
+
+        public ProjectItem Item {
+            get;
+            set;
+        } = null;
+
+        public FilterNode Parent {
+            get;
+            set;
+        } = null;
+
+        public FileNode(ProjectItem in_item) {
+            Item = in_item;
+            Path = in_item.EvaluatedInclude;
+            Name = System.IO.Path.GetFileName(in_item.EvaluatedInclude);
+        }
+
+        public FileNode() {
+            //
+        }
+    }
+}
---
+++ AutoFilteringCui/Program.cs
@@ -0,0 +1,9 @@
+﻿using AutoFilterginCui;
+using Microsoft.Build.Evaluation;
+
+string vcxprjPath = args[0];
+Project project = new Project(vcxprjPath);
+var projectControl = new ProjectControl(project);
+projectControl.Execute();
+
+return 0;
